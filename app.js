f4e7e8416a8cc834a615b01f4051634167081dfc
Rajiuddin <skrajiuddin18@gmail.com>
2024-11-14 12:44:35 +0530

    Added the create ticket page

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const hbs=require("hbs")
 const bodyParser = require("body-parser")
 const path = require("path")
 
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'src')));
 app.set("views", path.join(__dirname,"../src"))
+app.set("view engine", "hbs")
+app.set("views", path.join(__dirname,"hbs_templates"))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port,()=>{
@@ -21,18 +24,31 @@ app.get("/",(req,res)=>{
 app.get("/login-admin",(req,res)=>{
     res.sendFile(path.join(__dirname,"./src/login-admin.html"))
 })
-app.get("/login-user",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./src/login-user.html"))
-})
 app.post("/login-admin",async (req,res)=>{
     const user_id = req.body.admin_id;
     const user_pass=req.body.admin_pass;
     console.log(user_id)
     res.send("Successfully logged in!")
 })
+app.get("/login-user",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./src/login-user.html"))
+})
 app.post("/login-user",async (req,res)=>{
     const user_id = req.body.user_id;
     const user_pass=req.body.user_pass;
     console.log(user_id)
     res.send("Successfully logged in!")
+})
+app.get("/tickets-user",(req,res)=>{
+    res.render("tickets")
+})
+app.get("/create-ticket-page",(req,res)=>{
+    res.render("create-tickets-page")
+})
+app.post("/generate-tickets",async (req,res)=>{
+    const query=req.body.write_ticket;
+    res.send(query)
+})
+app.get("/track-ticket-page",(req,res)=>{
+    res.send("Hello")
 })
